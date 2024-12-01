6d99b750bee7c259af0ab48a9046baa86e2ebd9c
akayanuarcandra <hazumiya.yc@gmail.com>
2024-12-02 00:22:18 +0800

    refactor directory and add a lots of things

--- js/main-navbar.js
+++ js/main-navbar.js
@@ -1,10 +1,6 @@
-window.onload = function() {
-    window.scrollTo(0, 0);
-};
-
 window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
-    if (scroll > 1300) {
+    if (scroll > 100) {
         document.querySelector(".navigation").classList.add("shadow");
     } else {
         document.querySelector(".navigation").classList.remove("shadow");
